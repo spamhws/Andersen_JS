65fa20e5123c003b7a6dbb6553125ff2d86e6d79
spamhws <egorsanko@gmail.com>
2022-05-15 17:11:35 +0200

    homeword 4, upload 1

---
+++ ДЗ4/concatStrings_&_Calculator_Yehor_Sanko.js
@@ -0,0 +1,78 @@
+// Task 1, concatStrings function:
+
+let currentString = '';
+let currentSeparator = '';
+
+function concatStrings(string, separator) {
+  if (typeof string === 'string') {
+    currentString += string;
+
+    if (currentString !== '') {
+      if (typeof separator === 'string') {
+        currentSeparator = separator;
+        currentString += currentSeparator;
+      } else if (currentSeparator !== '') {
+        currentString += currentSeparator;
+      }
+    }
+    return concatStrings;
+  }
+
+  if (currentSeparator.length !== 0) {
+    currentString = currentString.slice(0, -currentSeparator.length);
+  }
+
+  let result = currentString;
+
+  currentString = '';
+  currentSeparator = '';
+
+  return result;
+}
+
+// Task 2, Calculator Class:
+
+function checkNumberValidity(input) {
+  let result = typeof input === 'number' && isFinite(input);
+  if (!result) {
+    throw new Error('Invalid parameters passed to the Class');
+  }
+}
+
+class Calculator {
+  constructor(numX, numY) {
+    checkNumberValidity(numX);
+    checkNumberValidity(numY);
+    this.numX = numX;
+    this.numY = numY;
+  }
+
+  setX = (num) => {
+    checkNumberValidity(num);
+    this.numX = num;
+  };
+
+  setY = (num) => {
+    checkNumberValidity(num);
+    this.numY = num;
+  };
+
+  logSum = () => {
+    console.log(this.numX + this.numY);
+  };
+
+  logMul = () => {
+    console.log(this.numX * this.numY);
+  };
+
+  logSub = () => {
+    console.log(this.numX - this.numY);
+  };
+
+  logDiv = () => {
+    if (this.numY === 0) {
+      throw new Error(`Second argument can't equal to 0`);
+    }
+    console.log(parseFloat((this.numX / this.numY).toFixed(2)));
+  };
+}
